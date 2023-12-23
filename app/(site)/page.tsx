8e9bf620c4f64be58d0928e6e065f0ea984db81e
Hanliying Xu <xu000295@algonquinlive.com>
2023-12-23 01:33:50 -0500

    Commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import getSongs from "@/actions/getSongs";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="
       bg-neutral-900
@@ -49,9 +53,7 @@ export default function Home() {
             Newest Songs
           </h1>
         </div>
-        <div>
-          List of Songs
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   )
